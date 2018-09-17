262025b5bb64ce45d3f8c2ef922ccc25ca8a5a9a
happycodingnow <happycodingnow@sina.com>
2018-09-17 12:05:20 +0800

    Initial commit

---
+++ src/test/java/com/huangguizhao/huangguizhaogitnew/HuangguizhaoGitNewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huangguizhao.huangguizhaogitnew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HuangguizhaoGitNewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
