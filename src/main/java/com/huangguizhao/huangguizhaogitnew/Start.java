8f169d3b59cb6571e4ef168b22af06e28636606a
happycodingnow <happycodingnow@sina.com>
2018-09-17 12:55:16 +0800

    添加2018

--- src/main/java/com/huangguizhao/huangguizhaogitnew/Start.java
+++ src/main/java/com/huangguizhao/huangguizhaogitnew/Start.java
@@ -4,5 +4,6 @@ public class Start {
     public static void main(String[] args){
         System.out.println("添加了main方法");
         System.out.println("2018到此一游");
+        System.out.println("2018");
     }
 }
